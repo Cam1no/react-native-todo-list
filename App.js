a23a7d8d73f1b8c5875ceeaeba706505ed34df40
joe <kinjoyuji@gmail.com>
2018-06-23 19:24:27 +0900

    login form

--- App.js
+++ App.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Button,
+  Text,
+  Footer,
+} from 'native-base';
 export default class App extends Component {
   render() {
     return (
       <Container>
         <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
+          <Text>Login Form</Text>
         </Header>
         <Content>
-          <Text>
-            This is Content Section
-          </Text>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
+          <Form>
+            <Item>
+              <Input placeholder='Username' />
+            </Item>
+            <Item last>
+              <Input placeholder='Password' />
+            </Item>
+            <Button primary full>
+              <Text> Primary </Text>
             </Button>
-          </FooterTab>
-        </Footer>
+          </Form>
+        </Content>
+        <Footer />
       </Container>
     );
   }
