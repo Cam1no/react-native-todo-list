8aeba5646198a2549fb861c004afda896381c02c
joe <kinjoyuji@gmail.com>
2018-06-24 21:48:07 +0900

    fix eslint

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Container, Header, Body, Title, Footer } from 'native-base';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
-import reducers from './src/reducers/';
-import RootNavigator from './src/RootNavigator';
-import thunk from 'redux-thunk';
 import firebase from 'firebase';
+import thunk from 'redux-thunk';
+import reducers from './src/reducers';
+import RootNavigator from './src/RootNavigator';
 import firebaseConfig from './src/config/firebase';
 
 firebase.initializeApp(firebaseConfig);
 
-const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)),);
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <RootNavigator/>
+        <RootNavigator />
       </Provider>
     );
   }
