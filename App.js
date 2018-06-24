b1a3f333ca777305499c8c8e1280943c1cc2d7c9
joe <kinjoyuji@gmail.com>
2018-06-24 16:53:37 +0900

    setting redux

--- App.js
+++ App.js
@@ -1,66 +1,25 @@
-import React, { Component } from 'react';
-import {
-  Container, Header,
-  Title, Body,
-  View, Content,
-  Form, Item,
-  Input, Button,
-  Text, Footer,
-} from 'native-base';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Container, Header, Body, Title, Footer } from 'native-base';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import reducers from './src/reducers/';
+// import RootNavigator from './src/RootNavigator';
+import thunk from 'redux-thunk';
 import firebase from 'firebase';
 import firebaseConfig from './src/config/firebase';
 
 firebase.initializeApp(firebaseConfig);
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      user: {},
-    }
-  }
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)),);
 
+export default class App extends React.Component {
   render() {
     return (
-      <Container>
-        <Header>
-          <Body>
-            <Title>Login Form</Title>
-          </Body>
-        </Header>
-        <Content>
-          <Form>
-            <Item>
-              <Input
-                placeholder='Email'
-                onChangeText={email => this.setState({ email })}
-              />
-            </Item>
-            <Item last>
-              <Input
-                placeholder='Password'
-                onChangeText={password => this.setState({ password })}
-              />
-            </Item>
-            <Button primary full onPress={() => this._handleSubmit()} >
-              <Text>Register</Text>
-            </Button>
-          </Form>
-        </Content>
-        <Footer />
-      </Container>
+      <Provider store={store}>
+        <View><Text>HelloWorld</Text></View>
+      </Provider>
     );
-  }
-
-  _handleSubmit = () => {
-    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(user => {
-        this.setState({ user: user, email :null, password: null });
-      })
-      .catch((error) => {
-        console.log("firebase error", error);
-      });
   }
 }
---
+++ src/reducers/currentUser.js
@@ -0,0 +1,15 @@
+const initialState = {
+  uid: '',
+  displayName: '',
+  email: '',
+  password: '',
+}
+
+const currentUser = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default currentUser;
